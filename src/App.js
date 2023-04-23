b1ae939a7bccc8765219974543c4478621cae5cb
Gladbird <96986060+Gladbird@users.noreply.github.com>
2023-04-23 12:46:56 +0000

    Final

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import {Routes, Route} from "react-router-dom";
 
+import PlayPage from './pages/Play.page';
+import HomePage from './pages/Home.page';
+import MoviePage from './pages/Movies.page';
+
 function App() {
   return (
     <Routes>
-    {/*<Route path="/"element={<HomePage/>}/>
-    <Route path="/movie/:id" />
-  <Route path="/plays"element={<Plays/>} />*/}
+      <Route path="/"element={<HomePage/>} />
+      <Route path="/movie/:id" element={<MoviePage/>} />
+      <Route path="/plays" element={<PlayPage/>} />
+
     </Routes> 
   );
 }
